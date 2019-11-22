105b38ec0358e8b85aa2e81ee33706e4967927eb
Tech Gyan <53473704+GyanTech877@users.noreply.github.com>
2019-11-22 23:07:26 +0530

    Create DeleteNodeInMiddle.java

---
+++ DeleteNodeInMiddle.java
@@ -0,0 +1,30 @@
+/*
+Implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node.
+*/
+
+
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */
+
+
+public class Solution {
+    /*
+     * @param node: the node in the list should be deleted
+     * @return: nothing
+     */
+    public void deleteNode(ListNode node) {
+        if(node == null || node.next == null) return;
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
