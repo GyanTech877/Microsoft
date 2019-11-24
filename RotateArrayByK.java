b0b93fac136fe01fb6d38182dea2dea4785b5619
Tech Gyan <53473704+GyanTech877@users.noreply.github.com>
2019-11-25 00:49:51 +0530

    Update RotateArrayByK.java

--- RotateArrayByK.java
+++ RotateArrayByK.java
@@ -68,3 +68,32 @@ public class Solution3 {
         }
     }
 }
+
+public class Solution4 {
+    /**
+     * @param nums: an array
+     * @param k: an integer
+     * @return: rotate the array to the right by k steps
+     */
+    public int[] rotate(int[] nums, int k) {
+        if(nums==null || nums.length==0||(k%nums.length)==0) return nums;
+        int n = nums.length;
+        k=k%n;
+        int count = 0;
+        for(int i=0;i<n && count<n;i++){
+            int start=i;
+            int current =i;
+            int prevValue = nums[i];
+            while(true){
+                int next = (current + k)%n;
+                int currentValue = nums[next];
+                nums[next] = prevValue;
+                prevValue = currentValue;
+                current = next;
+                count++;
+                if(current==start || count == n) break;
+            }
+        }
+        return nums;
+    }
+}
